53df6687b38ac02f0b364c71a1043e45029588e6
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-07-18 00:13:46 +0300

    Add usage cases

--- JavaScript/8-promisify.js
+++ JavaScript/8-promisify.js
@@ -20,11 +20,24 @@ const main = async () => {
   const fileName = '8-promisify.js';
   const data = await read(fileName, 'utf8');
   console.log(`File "${fileName}" size: ${data.length}`);
+
+  // Call from try/catch block
   try {
     const data = await read('unknown.file', 'utf8');
     console.log(`File size: ${data.length}`);
   } catch (error) {
     console.error(error.message);
+  }
+
+  // Set default value in promise.catch block
+  const content = await read('unknown.file').catch((error) => {
+    console.error(error.message);
+    return null;
+  });
+  // Warning: note that you need additional if-statement
+  if (content) {
+    console.log({ content });
+    console.log(`File size: ${content.size}`);
   }
 };
 
